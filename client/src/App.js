f4c670cc3e7b046473ceb3db22a9285b41468357
Sean Cortes <cortess@oregonstate.edu>
2018-07-20 19:03:14 -0700

    Implemented dropdown box for list of monsters. App now queries hitzones.

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,37 @@ import './App.css';
 
 class App extends Component {
   state = {
-    weapons: []
+    weapons: [],
+    monsters: [],
+    weaponValue: 'None',
+    monsterValue: 'None'
   };
 
   componentDidMount() {
     this.getWeapons();
+    this.getMonsters();
   }
 
   getWeapons = _ => {
-    fetch('http://localhost:3000/test')
+    fetch('http://localhost:3000/weapon')
       .then(response => response.json())
       .then(response => this.setState({ weapons: response.data }))
       .catch(err => console.error(err))
   }
 
-  renderWeapons = ({name}) => <option key={name}>{name}</option>
+  getMonsters = _ => {
+    fetch('http://localhost:3000/monster')
+      .then(response => response.json())
+      .then(response => this.setState({ monsters: response.data }))
+      .catch(err => console.error(err))
+  }
 
-  testChange = (event) => {
-  	fetch('http://localhost:3000/test', {
+  renderWeapons = ({ weapon_id, name }) => <option key={weapon_id} id={weapon_id}>{name}</option>
+
+  renderMonsters = ({ monster_id, name }) => <option key={monster_id} id ={monster_id}>{name}</option>
+
+  weaponTypeSelect = (event) => {
+  	fetch('http://localhost:3000/weapon', {
   		method: 'POST',
   		headers: {
   			'Content-Type': 'application/json'
@@ -35,8 +48,23 @@ class App extends Component {
   	  .catch(err => console.error(err))
   }
 
+  weaponSelect = (event) => {
+  	this.setState({
+  		weaponValue: event.target.value
+  	})
+  }
+
+  monsterSelect = (event) => {
+    this.setState({
+      monsterValue: event.target.value
+    })
+  }
+
   render() {
     const { weapons } = this.state;
+    const { monsters } = this.state;
+    const { weaponValue } = this.state;
+    const { monsterValue } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -44,16 +72,83 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <select onChange={this.testChange}>
+          <select onChange={this.weaponTypeSelect}>
+          	<option value="0">All</option>
           	<option value="1">Great Sword</option>
           	<option value="2">Long Sword</option>
           	<option value="3">Sword and Shield</option>
           </select>
-          <select>
+          <select onChange={this.weaponSelect}>
+            <option value="None">None</option>
           	{weapons.map(this.renderWeapons)}
           </select>
+          <select onChange={this.monsterSelect}>
+            <option value="None">None</option>
+            {monsters.map(this.renderMonsters)}
+          </select>
         </p>
+        <MonsterData wep={weaponValue} mon={monsterValue}/>
       </div>
+    );
+  }
+}
+
+
+class MonsterData extends Component {
+  state={
+    hitzone: []
+  }
+
+  componentDidUpdate(prevProps){
+    if(this.props.wep !== prevProps.wep || this.props.mon !== prevProps.mon){
+      this.getHitZones(this.props.mon)
+    }
+
+  }
+
+  getHitZones = (prop) => {
+    fetch('http://localhost:3000/hitzone', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: prop
+      })
+    })
+    .then(response => response.json())
+    .then(response => this.setState({ hitzone: response.data }))
+    .catch(err => console.error(err))
+  }
+
+  renderHitZones = ({ monster_id, name, part, sever, blunt, shot, fire, water, thunder, ice, dragon, stun}) =>
+    <tr key={monster_id}> 
+      <td key={monster_id + 'part'}>{part}</td>
+      <td key={monster_id + 'sever'}>{sever}</td>
+      <td key={monster_id + 'blunt'}>{blunt}</td>
+      <td key={monster_id + 'shot'}>{shot}</td>
+      <td key={monster_id + 'fire'}>{fire}</td>
+      <td key={monster_id + 'water'}>{water}</td>
+      <td key={monster_id + 'thunder'}>{thunder}</td>
+      <td key={monster_id + 'ice'}>{ice}</td>
+      <td key={monster_id + 'dragon'}>{dragon}</td>
+      <td key={monster_id + 'stun'}>{stun}</td>
+    </tr>
+  
+
+  render(){
+    const { hitzone } = this.state;
+    return(
+	  <div>
+	    <p>{this.props.wep} </p>
+	    <p>{this.props.mon}</p>
+      <table>
+        <tbody>
+          {hitzone.map(this.renderHitZones)}
+        </tbody>
+      </table>
+      
+	  </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.set('port', 5000);
 
-app.get('/test', function(req, res){
+app.get('/weapon', function(req, res){
   db.query('SELECT *  FROM `weapon`', function(err, results){
     if (err){
       return res.send(err);
@@ -24,24 +24,61 @@ app.get('/test', function(req, res){
         data: results
       })
     }
-    
   })
 });
 
-app.post('/test', function(req, res){
-  db.query('SELECT name FROM `weapon` WHERE weapon_class = ?', 
-    [req.body.class], function(err, results, fields){
+
+app.post('/weapon', function(req, res){
+  if(req.body.class==0){
+    db.query('SELECT * FROM `weapon`', function(err, results){
+      if (err){
+        return res.send(err);
+      }
+      else{
+        return res.json({ data: results })
+      }
+    })
+  }
+  else{
+    db.query('SELECT weapon_id, name FROM `weapon` WHERE weapon_class = ?',
+      [req.body.class], function(err, results, fields){
+        if(err){
+          return res.send(err);
+        }
+        else {
+          return res.json({ data: results })
+        }
+      })
+  }
+});
+
+app.get('/monster', function(req, res){
+  db.query('SELECT * FROM `monster`', function(err, results){
     if(err){
       return res.send(err);
     }
-    else {
-      return res.json({
-        data: results
-      })
+    else{
+      return res.json({ data: results })
     }
   })
-});
+})
 
+
+app.post('/hitzone', function(req, res){
+  // console.log(req.body)
+  var context={}
+  db.query('SELECT * FROM `monster` WHERE name = ?',
+    [req.body.name], function(err, results){
+      if(err){
+        return res.send(err);
+      }
+      else{
+        return res.json({
+          data: results
+        })
+      }
+    })
+})
 
 app.listen(app.get('port'), function(){
   console.log('Express started on port:' + app.get('port') + '; press Ctrl-C to terminate.');
