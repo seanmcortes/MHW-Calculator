54d57708cc6583d66f1299d17228890f2707e391
Sean Cortes <cortess@oregonstate.edu>
2018-07-24 17:42:42 -0700

    added minimum calculations

--- client/src/App.js
+++ client/src/App.js
@@ -127,9 +127,61 @@ class MonsterData extends Component {
   }
 
   calcDamage = (sever, blunt, shot, fire, water, thunder, ice, dragon, stun) => {
-    console.log(sever)
+    var weapon = this.props.wep;
+
+    var severDamage, bluntDamage, shotDamage, stunDamage, totalDamage = 0;
+
+    if(weapon.weapon_class == 5 || weapon.weapon_class == 6){
+      var bluntDamage = Math.ceil((weapon.real_damage * (1 + 0.25 * (weapon.weapon_affinity/100))) * (blunt/100));
+      totalDamage += bluntDamage;
+    } else {
+      var severDamage = Math.ceil((weapon.real_damage * (1 + 0.25 * (weapon.weapon_affinity/100))) * (sever/100));
+      totalDamage += severDamage;
+    }
+
+    var trueElement = weapon.element_damage/10;
+
+    switch(weapon.element_type){
+      case "":
+        break;
+      case "Fire":
+        var fireDamage = Math.ceil(trueElement * (fire/100));
+        totalDamage += fireDamage;
+        break;
+      case "Water":
+        var waterDamage = Math.ceil(trueElement * (water/100));
+        totalDamage += waterDamage;
+        break;
+      case "Thunder":
+        var thunderDamage = Math.ceil(trueElement * (thunder/100));
+        totalDamage += thunderDamage;
+        break;
+      case "Ice":
+        var iceDamage = Math.ceil(trueElement * (ice/100));
+        totalDamage += iceDamage;
+        break;
+      case "Dragon":
+        var dragonDamage = Math.ceil(trueElement * (dragon/100));
+        totalDamage += dragonDamage;
+        break;
+    }
+
     if(this.props.wep == undefined){console.log("props fail!")}
-    return <td>{sever}</td>
+    return (
+      <tr>
+        <td></td>
+        <td>{severDamage}</td>
+        <td>{bluntDamage}</td>
+        <td>{shotDamage}</td>
+        <td>{fireDamage}</td>
+        <td>{waterDamage}</td>
+        <td>{thunderDamage}</td>
+        <td>{iceDamage}</td>
+        <td>{dragonDamage}</td>
+        <td>{stunDamage}</td>
+        <td>{totalDamage}</td>
+      </tr>
+    )
   }
   
 
@@ -148,9 +200,7 @@ class MonsterData extends Component {
       <td key={monster_part_id + 'dragon'}>{dragon}</td>
       <td key={monster_part_id + 'stun'}>{stun}</td>
     </tr>
-    <tr>
-      {this.calcDamage(sever, blunt, shot, fire, water, thunder,  ice, dragon, stun)}
-    </tr>
+    {this.calcDamage(sever, blunt, shot, fire, water, thunder,  ice, dragon, stun)}
     </tbody>
   
 
@@ -158,9 +208,20 @@ class MonsterData extends Component {
     const { hitzone } = this.state;
     return(
 	  <div>
-      <p>{this.state.wep}</p>
-	    <p>{this.props.mon}</p>
       <table>
+        <tr>
+          <th>Body Part</th>
+          <th>Sever</th>
+          <th>Blunt</th>
+          <th>Shot</th>
+          <th>Fire</th>
+          <th>Water</th>
+          <th>Thunder</th>
+          <th>Ice</th>
+          <th>Dragon</th>
+          <th>Stun</th>
+          <th>Total</th>
+        </tr>
         {hitzone.map(this.renderHitZones)}
       </table>
       
