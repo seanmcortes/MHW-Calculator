f6ff9dcdcb7f4f7d3c30a8cc9e777c6e37000254
Sean Cortes <cortess@oregonstate.edu>
2018-07-31 15:59:42 -0700

    adjust .gitignore and shrinkwrap

---
+++ client/src/Hitzone.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Hitzone extends Component{
+  constructor(props){
+    super(props);
+  }
+  
+  state={}
+
+  render(){
+    return(
+        <div className="Hitzone">
+          <p>testing hitzone</p>
+        </div>
+    );
+  }
+}
+
+export default Hitzone;
